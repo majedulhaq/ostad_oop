a85f01fd306578220a629a218e550cb135e8f656
88018 <majedulhaq550@gmail.com>
2024-06-24 23:37:55 +0600

    Merge remote-tracking branch 'origin/main'

--- live_test.dart
+++ live_test.dart
@@ -1,32 +1,24 @@
-abstract class computer {
-  String name;
-  int model;
-  int year;
-
-  computer({
-    required this.name,
-    required this.model,
-    required this.year,
-  });
-}
-
-class hp extends computer {
-  hp({
-    required int model,
-    required int year,
-  }) : super(
-          name: 'hp',
-          model: model,
-          year: year,
-        );
-
-  void startComp() {
-    print(
-        'this is name of my  ${name} laptop and its model  ${model} and it is launched on ${year} ');
+class Media {
+  void play() {
+    print('Playing media...');
   }
 }
 
-main() {
-  hp myComp = hp(model: 4500, year: 2012);
-  myComp.startComp();
+class Song extends Media {
+  String artist;
+
+  Song(this.artist);
+
+  @override
+  void play() {
+    print('Playing song by $artist');
+  }
+}
+
+void main() {
+  Media media = Media();
+  media.play();
+
+  Song song = Song('Khalid Hasan Milu');
+  song.play();
 }
